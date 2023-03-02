b28715d8cd4297922ae8a71243ae4ef75430f761
userforawhile <118730073+userforawhile@users.noreply.github.com>
2023-03-02 14:49:03 +0100

    Delete esercizi directory

--- esercizi/for loops.py
+++
@@ -1,4 +0,0 @@
-meals = ['pasta,carne,pizza,insalata']
-
-for meal in 'meals':
-    print(meal.capitalize())
--- esercizi/while loop.py
+++
@@ -1,7 +0,0 @@
-password = input("enter a password:")
-while password != 'pass123':
-     password = input("enter a password:")
-print('password was correct')
-
-
-
