afad3836cd3a08c99768edaccc03f55b4c631c2f
Antonio Marino <marino.ant@yahoo.com>
2023-03-02 14:02:06 +0100

    Create basi GUI without interactions day 16th

--- gui.py
+++ gui.py
@@ -3,9 +3,9 @@ import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip=[""])
-add_botton = sg.Button('Add')
+add_button = sg.Button('Add')
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_botton]])
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
 
 window.read()
 window.close()
